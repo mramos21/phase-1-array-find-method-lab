6e617519fb2d53c6068bdc9ebc231785ed5b4e0b
Michael Ramos <ramosmichael@live.com>
2022-02-16 04:09:20 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(array) {
+    if (array.find(array => array.result === 'W')) {
+        let win = array.find(array => array.result === 'W')
+        return win.year
+    }
+}
